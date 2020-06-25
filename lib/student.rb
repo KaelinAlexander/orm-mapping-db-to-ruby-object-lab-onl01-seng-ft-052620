83d2919fdabf304d1aa8c426500242063a55cc35
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-25 16:35:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
     WHERE grade = 10
     LIMIT 1
     SQL
-    student= DB[:conn].execute(sql).map do |row|
+    student = DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)[0]
     end
     student
